c272a2be300cea131149f8b0d707135b1e13ad6b
jahidul2004 <islamjahiduljihad@gmail.com>
2025-02-08 20:02:56 +0600

    Simple test setup compleated.

--- index.js
+++ index.js
@@ -1,16 +1,34 @@
-const express = require('express');
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
-dotenv = require('dotenv').config();
+require("dotenv").config();
 
+// Create a new instance of the GoogleGenerativeAI class
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
 
+// Test the AI model
+app.get("/test-ai", async (req, res) => {
+    const prompt = "Explain how AI works in Bengali";
 
-app.get('/', (req, res) => {
+    const result = await model.generateContent(prompt);
+    console.log(result.response.text());
+
     res.send({
-        msg:"Crack the power of AI server is running"
+        prompt: prompt,
+        response: result.response.text(),
     });
-})
+});
 
+// Server status
+app.get("/", (req, res) => {
+    res.send({
+        msg: "Crack the power of AI server is running",
+    });
+});
+
+// Port listener
 app.listen(port, () => {
     console.log("Ai is running on port: " + port);
-})
+});
